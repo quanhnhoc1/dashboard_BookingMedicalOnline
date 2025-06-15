d8f57082d62e81ccd46f73273659872db12cca3d
MinhThan25 <quanhnhoc@gmail.com>
2025-06-15 13:22:04 +0700

    change the name file

---
+++ src/pages/medical-facility/PublicHospitalPage.vue
@@ -0,0 +1,8 @@
+<!-- src/pages/medical-facility/private-hospital.vue -->
+<template>
+  <div class="text-blue-500">Đây là trang bệnh viện cong</div>
+</template>
+
+<script setup>
+// Không cần import layout ở đây
+</script>
